1d286f85f8f922eb65efdea1b4ec278ae2af88f3
divaychauhan <divaypratap5113@gmail.com>
2026-05-02 17:43:44 +0530

    some changes 2 may

--- assingement/first.py
+++
@@ -1,8 +0,0 @@
-num1=20
-num2=30
-print("before swaping the numbers")
-print(num1,num2)
-num1,num2 = num2,num1
-print("After swapping the number")
-print("num1=",num1)
-print("num2=",num2)
---
+++ assingement/number-swapping.py
@@ -0,0 +1,22 @@
+num1=20
+num2=30
+print("before swaping the numbers")
+print("num1=", num1, "num2=",num2)
+num1,num2 = num2,num1
+print("After swapping the number")
+print("num1=",num1)
+print("num2=",num2)
+
+#with the help of another variable
+
+n1=20
+n2=30
+
+print(f"after swapping value of num1: {n1}")
+print(f"after swapping value of num1: {n2}")
+x=n1
+n1=n2
+n2=x
+print("--"*10)
+print(f"after swapping value of num1: {n1}")
+print(f"after swapping value of num1: {n2}")
